92e8899eee987b0fa00447e73f6d6dacf94907d3
Jefferson Dantas <52979246+josejefferson@users.noreply.github.com>
2024-04-05 13:31:32 +0000

    Commit

--- sistema/app/admin.py
+++ sistema/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Endereco, Produto
+from .models import Endereco, Fornecedor, Produto
 
 # Register your models here.
 admin.site.register(Produto)
+admin.site.register(Fornecedor)
 admin.site.register(Endereco)
--- sistema/app/models.py
+++ sistema/app/models.py
@@ -3,8 +3,22 @@ from django.db import models
 # Create your models here.
 class Produto(models.Model):
     nome = models.CharField(max_length=200)
-    criado = models.DateTimeField(auto_now_add=True)
+    fabricante = models.CharField(max_length=200)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data_validade = models.DateField()
+    estoque = models.IntegerField()
+    descricao = models.TextField()
 
+    def __str__(self):
+        return self.nome
+
+class Fornecedor(models.Model):
+    nome = models.CharField(max_length=200)
+    cpf_cnpj = models.CharField(max_length=18)
+    endereco = models.CharField(max_length=200, blank=True, null=True)
+    telefone = models.CharField(max_length=200)
+    obs = models.TextField()
+    
     def __str__(self):
         return self.nome
 
